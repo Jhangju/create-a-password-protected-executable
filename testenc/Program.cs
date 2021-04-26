eb92cde41be7b695f37a8dd819848d9b5f45877c
SANE <83189731+white-collor@users.noreply.github.com>
2021-04-26 15:14:43 +0500

    Add files via upload

---
+++ testenc/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testenc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = "";
+            for (int i = 0; i < args.Length; i++)
+            {
+                n += args[i];
+            }
+            if (n == "123")
+            {
+                Process cmd = new Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
+
+                cmd.StandardInput.WriteLine("start cmd");
+                cmd.StandardInput.Flush();
+                cmd.StandardInput.Close();
+                cmd.WaitForExit();
+                Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+            }
+            
+            else
+            Console.WriteLine("Not valid user");
+            Console.ReadLine();
+        }
+    }
+}
